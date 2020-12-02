3862d025acb10f29aaa805f17768e9d57c4de378
junkatou <jkky0225@gmail.com>
2020-12-02 21:43:58 +0900

    ChainOfRepository

---
+++ ChainOfRepository/src/NoSupport.java
@@ -0,0 +1,11 @@
+
+public class NoSupport extends Support{
+	public NoSupport(String name) {
+		super(name);
+	}
+
+	public boolean resolve(Trouble trouble) {
+		return false;
+	}
+
+}
---
+++ ChainOfRepository/src/Support.java
@@ -0,0 +1,42 @@
+public abstract class Support {
+	private String name;
+
+	/** たらいまわしの先 **/
+	private Support next;
+	public Support(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * たらいまわしの先を設定
+	 * @param next
+	 * @return
+	 */
+	public Support setNext(Support next) {
+		this.next = next;
+		return next;
+	}
+
+	public final void support(Trouble trouble) {
+		if (resolve(trouble)) {
+			done(trouble);
+		} else if (next != null) {
+			next.support(trouble);
+		} else {
+			fail(trouble);
+		}
+	}
+
+	public String toString() {
+		return "[" + name + "]";
+	}
+
+	protected abstract boolean resolve(Trouble trouble);
+	protected void done(Trouble trouble) {
+		System.out.println(trouble + " is resolved by " + this + ".");
+	}
+
+	protected void fail(Trouble trouble) {
+		System.out.println(trouble + " cannot be resolved.");
+	}
+}
